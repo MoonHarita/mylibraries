c6e5a1187cfb3784d86eaf58761ec74c63f5bed6
MoonHarita <101087139+MoonHarita@users.noreply.github.com>
2022-03-17 19:07:22 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -2,11 +2,24 @@ def newGit(repo)
 {
   git "${repo}"
 }
-def newBuild()
+
+def newBuild() 
 {
   sh 'mvn package'
-}  
+}
+
 def newDeploy(ip,appname)
 {
- deploy adapters: [tomcat9(credentialsId: '709852eb-f517-4ba1-815b-05a5b7dbe495', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'  
+  deploy adapters: [tomcat9(credentialsId: '709852eb-f517-4ba1-815b-05a5b7dbe495', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
+}
+
+def newTest(workspace)
+{
+  echo "${workspace}"
+  sh "java -jar /home/ubuntu/.jenkins/workspace/${workspace}/testing.jar"
+}
+
+def newEmail(email,subject,body)
+{
+  mail bcc: '', body: "${body}", cc: '', from: '', replyTo: '', subject: "${subject}", to: "${email}"
 }
